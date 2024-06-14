0cd3eb5fab142f4958ee934a875d04229375f326
d972303 <163311974+d972303@users.noreply.github.com>
2024-06-14 19:20:11 +0900

    0614 19:20

--- Home/script/script.js
+++ Home/script/script.js
@@ -2,3 +2,17 @@ function rotateText() {
   var btn = document.querySelector('.main--content-button');
   btn.classList.toggle('rotate'); // 버튼에 rotate 클래스를 토글링하여 회전 효과를 적용
 }
+
+// 참고자료
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggleButton');
+  const hiddenContent = document.getElementById('hiddenContent');
+
+  toggleButton.addEventListener('click', () => {
+    if (hiddenContent.classList.contains('hidden')) {
+      hiddenContent.classList.remove('hidden');
+    } else {
+      hiddenContent.classList.add('hidden');
+    }
+  });
+});
